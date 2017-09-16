cebd48ef9207a3a323b523931ff47e15f4781e47
Daniel <daniel-sj77@hotmail.com>
2017-09-15 20:19:27 -0400

    DBManager

--- app/src/main/java/com/das/daniel/ventalocal/DB/DBManager.java
+++ app/src/main/java/com/das/daniel/ventalocal/DB/DBManager.java
@@ -1,7 +1,12 @@
 package com.das.daniel.ventalocal.DB;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.das.daniel.ventalocal.Modelo.Producto;
+
+import java.util.List;
 
 /**
  * Created by Daniel on 14/09/2017.
@@ -41,6 +46,13 @@ public abstract class DBManager {
     abstract public void actualizarProducto(String id, String descripcion, String cantidad, String precio,
                                            String tipo );
     abstract public void darBajaProducto(String id, String estado );
+    abstract public void actualizarCantidad(String id, String cantidad );
+
+    abstract public Cursor cargarCursor();
+    abstract public Cursor cargarCursorBuscar(String descripcion);
+
+    abstract public List<Producto> getProductosList();
+    abstract public List<Producto> Buscar();
 
 
 
